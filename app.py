e50649c34cd4261399a8cb6c29271699d144f3b6
Malztt <105033698+Malztt@users.noreply.github.com>
2025-06-27 13:53:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ creds = Credentials.from_service_account_info(
     st.secrets["gcp_service_account"], scopes=scope
 )
 client = gspread.authorize(creds)
-sheet = client.open_by_key("16QNx4xaRjgvPnimZvS5nVA8HPcDTWg98QXKnCgWa7Xw").worksheet("A1")
+sheet = client.open_by_key("16QNx4xaRjgvPnimZvS5nVA8HPcDTWg98QXKnCgWa7Xw").sheet1
 # ข้อมูลทั่วไป
 col1, col2, col3 = st.columns(3)
 with col1:
